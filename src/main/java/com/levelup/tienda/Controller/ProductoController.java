7ba0441930d2876a24cec0378f831f98327c6543
Juan Carvajal Núñez <ju.carvajaln@duocuc.cl>
2026-01-24 22:57:59 -0300

    Add CRUD endpoints to ProductoController
    Implemented endpoints for retrieving a product by ID, updating, and deleting products. Enhanced logging for product creation, update, and deletion actions. This improves the controller to support full CRUD operations for Producto entities.

--- src/main/java/com/levelup/tienda/Controller/ProductoController.java
+++ src/main/java/com/levelup/tienda/Controller/ProductoController.java
@@ -17,14 +17,55 @@ public class ProductoController {
         this.repo = repo;
     }
 
+    // Listar producto
     @GetMapping
     public List<Producto> listar() {
         return repo.findAll();
     }
 
+    // Obtener producto por ID
+    @GetMapping("/{id}")
+    public Producto obtenerPorId(@PathVariable Integer id) {
+        return repo.findById(id).orElse(null);
+    }
+
+    // Crear el producto
     @PostMapping
     public Producto crear(@RequestBody Producto producto) {
-        System.out.println("Recibido: " + producto.getNombre());
+        System.out.println("Producto Recibido: " + producto.getNombre());
         return repo.save(producto);
     }
+
+    // Actualizar producto
+    @PutMapping("/{id}")
+    public Producto actualizar(
+        @PathVariable Integer id,
+        @RequestBody Producto datos
+    ) {
+        Producto producto = repo.findById(id).orElse(null);
+
+        if (producto == null) return null;
+
+        producto.setNombre(datos.getNombre());
+        producto.setPrecio(datos.getPrecio());
+        producto.setImagen(datos.getImagen());
+        producto.setDescripcion(datos.getDescripcion());
+
+        System.out.println("Producto Actualizado: " + producto.getNombre() 
+        + "\nId: " + producto.getId());
+        return repo.save(producto);
+    }
+
+    // Eliminar Producto
+    @DeleteMapping("/{id}")
+    public void eliminar(@PathVariable Integer id) {
+        Producto producto = repo.findById(id).orElse(null);
+        if (producto != null){
+            repo.deleteById(id);
+            System.out.println("Producto Eliminado: " + producto.getNombre() 
+            + "\nId: " + producto.getId()) ;
+        }
+        
+    }
+
 }
